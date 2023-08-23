d70c2fd7a6811de7fb8e930940d32691844c50da
Ryan Gomes <ryaanreeal@outlook.com>
2023-08-22 21:52:47 -0300

    v1.3.2.230822
    - Modificações na dashboard.

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
 <img alt="MySQL" src="https://img.shields.io/badge/MySQL-%2300f.svg?&logo=MySQL&logoColor=white">
 </a>
 
-<a href=""><img src="https://img.shields.io/badge/version-1.2.2-230819?" height="22" alt="Version"/></a>
+<a href=""><img src="https://img.shields.io/badge/version-1.3.2-230822?" height="22" alt="Version"/></a>
 
 <br>
 
@@ -32,6 +32,12 @@
 ## Notas de versão
 
 </a>
+
+### v1.3.2.230822
+
+- Modificações na dashboard.
+
+##
 
 ### v1.2.2.230819
 
--- pages/dashboard.php
+++ pages/dashboard.php
@@ -25,22 +25,130 @@ include("../assets/config/auth_session.php");
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
 </head>
 
-<body>
+<body class="colorBG">
     <nav>
         <div class="navback">
             <div class="userinfo">
-                <div id="usericon" onclick="myFunction()">
+                <div id="usericon">
                     <img class="userimg" src="../assets/img/user-icon.png">
                 </div>
                 <div id="usertext">
                     <p>Olá, <?php echo $_SESSION['email']; ?></p>
                 </div>
             </div>
-            <div class="logoutButton">
-                <p><a href="../assets/config/logout.php">Sair</a></p>
+            <div class="buttons">
+                <div class="sendButton">
+                    <p><a href="">Enviar</a></p>
+                </div>
+                <div class="logoutButton">
+                    <p><a href="../assets/config/logout.php">Sair</a></p>
+                </div>
+            </div>
+    </nav>
+
+    <main>
+        <div class="dashboardContainer">
+            <div class="parent">
+                <div class="card">
+                    <div id="cardTitle">Recurso Chave</div>
+                    <div id="cardSubtitle">
+                        São as empresas ou organizações que ajudam a empresa a criar valor para o cliente.
+                    </div>
+                </div>
+
+                <div class="card_green">
+                    <div id="cardTitle" class="mt">Proposta de Valor</div>
+                    <div id="cardSubtitle" class="mb">
+                        É o valor que o produto/serviço oferece para o cliente.
+                    </div>
+                </div>
+
+                <div class="card_aquamarine">
+                    <div id="cardTitle" class="mt">Segmento de Cliente</div>
+                    <div id="cardSubtitle" class="mb">
+                        São os grupos de clientes que a empresa pretende atender.
+                    </div>
+                </div>
+
+                <div class="card_olive">
+                    <div id="cardTitle">Parceiros Chave</div>
+                    <div id="cardSubtitle">
+                        São as empresas ou organizações que ajudam a empresa a criar valor para o cliente.
+                    </div>
+                </div>
+
+                <div class="card_yellow">
+                    <div id="cardTitle">Problemas</div>
+                    <div id="cardSubtitle">
+                        São as necessidades ou desafios enfrentados pelo cliente que a empresa se propõe a solucionar.
+                    </div>
+                </div>
+
+                <div class="card_red">
+                    <div id="cardTitle">Solução</div>
+                    <div id="cardSubtitle">
+                        É o produto/serviço oferecido pela empresa para resolver os problemas do cliente.
+                    </div>
+                </div>
+
+                <div class="card_emerald">
+                    <div id="cardTitle" class="mt">Relacionamento com cliente</div>
+                    <div id="cardSubtitle" class="mb">
+                        É a forma como a empresa se relaciona com o cliente.
+                    </div>
+                </div>
+
+                <div class="card_rose">
+                    <div id="cardTitle" class="mt">Atividades Chaves</div>
+                    <div id="cardSubtitle" class="mb">
+                        São as atividades essenciais para a operação do negócio.
+                    </div>
+                </div>
+
+                <div class="card_orange">
+                    <div id="cardTitle">Métricas Chave</div>
+                    <div id="cardSubtitle">
+                        São as métricas utilizadas para mensurar o desempenho do negócio.
+                    </div>
+                </div>
+
+                <div class="card_jade">
+                    <div id="cardTitle">Canais de distribuição</div>
+                    <div id="cardSubtitle">
+                        São os canais pelos quais a empresa se comunica e entrega seu produto/serviço ao cliente.
+                    </div>
+                </div>
+
+                <div class="card_lightblue">
+                    <div id="cardTitle" class="mt">Estrutura de custo</div>
+                    <div id="cardSubtitle" class="mb">
+                        São os custos envolvidos na operação do negócio.
+                    </div>
+                </div>
+
+                <div class="card_lawngreen">
+                    <div id="cardTitle" class="mt">Vantagem competitiva</div>
+                    <div id="cardSubtitle" class="mb">
+                        É o que diferencia o negócio dos seus concorrentes.
+                    </div>
+                </div>
+
+            </div> <!--- Div Grid --->
+            <div class="cardBottom">
+                <div class="card_gray">
+                    <div id="cardTitle" class="mt">Fonte de receita</div>
+                    <div id="cardSubtitle" class="mb">
+                        É como a empresa ganha dinheiro.
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
+    </main>
+    <div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+    </div>
 </body>
 
 </html>
